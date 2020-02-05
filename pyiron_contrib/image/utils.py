82f6cb6f987e7cff42cd895e9ce754edaefa57e1
Liam Huber <liam.huber@gmail.com>
2020-02-05 10:16:11 +0100

    Allow selectively scraping different components from a module.
    Sometimes you might only want to find, e.g., the classes defined there.

--- pyiron_contrib/image/utils.py
+++ pyiron_contrib/image/utils.py
@@ -24,44 +24,68 @@ __date__ = "Feb 3, 2020"
 
 class ModuleScraper:
     """
-    A class which scrapes through a module and applies primitives found as attributes of itself, functions found as
-    methods of itself, and sub-modules found recursively as new `ModuleScraper` attributes of itself.
+    A class which scrapes through a module and applies classes and primitives found as attributes of itself, functions
+    found as methods of itself, and sub-modules found recursively as new `ModuleScraper` attributes of itself.
 
     A decorator can optionally be applied to all functions found.
 
     Note:
         Doesn't do anything until its `activate` method is called.
 
-    Note:
-        Skips all module values beginning with an underscore unless activated with.
-
-    Warning:
-        Doesn't do anything with classes that are found.
+    Attributes:
+        safe (bool): Whether to skip values beginning with an underscore. (Default is True, do skip.)
+        recursive (bool): Whether to recursively activate submodules. (Default is False: submodules are noted by
+            creating a new `ModuleScraper` instance and setting it as an attribute, but contents cannot be accessed
+            until the submodule itself is explicitly activated.)
+        scrape_functions (bool): Whether to look for functions in the module. (Default is True.)
+        scrape_classes (bool): Whether to look for class definitions in the module. (Default is True.)
+        scrape_primitives (bool): Whether to look for primitives in the module. (Default is True.)
+        primitives_list (tuple/list): A list of which types count as primitive. (Default is None, which uses `(int,
+            float, bool, numpy.ndarray)`.)
     """
 
-    def __init__(self, module, decorator=None, decorator_args=None, scrape_methods=True):
+    def __init__(
+            self,
+            module,
+            decorator=None,
+            decorator_args=None,
+            safe=True,
+            recursive=True,
+            scrape_functions=True,
+            scrape_classes=True,
+            scrape_primitives=True,
+            primitives_list=None
+    ):
         """
         Args:
             module (module/str): The module from which to scrape, or the name of the module from which to escape, e.g.
                 `skimage.filters` or `'skimage.filters'`.
             decorator (fnc): A decorator function to apply to scraped functions. (Default is None, no decorator.)
             decorator_args (tuple/list): Arguments to pass to the decorator. (Default is None, no args.)
+            safe (bool): Whether to skip values beginning with an underscore. (Default is True, do skip.)
+            recursive (bool): Whether to recursively activate submodules. (Default is False: submodules are noted by
+                creating a new `ModuleScraper` instance and setting it as an attribute, but contents cannot be accessed
+                until the submodule itself is explicitly activated.)
+            scrape_functions (bool): Whether to look for functions in the module. (Default is True.)
+            scrape_classes (bool): Whether to look for class definitions in the module. (Default is True.)
+            scrape_primitives (bool): Whether to look for primitives in the module. (Default is True.)
+            primitives_list (tuple/list): A list of which types count as primitive. (Default is None, which uses `(int,
+                float, bool, numpy.ndarray)`.)
         """
         self._module = module
         self._decorator = decorator
         self._decorator_args = decorator_args or ()
         self._activated = False
-        self._scrape_methods = scrape_methods
+        self.safe = safe
+        self.recursive = recursive
+        self.scrape_functions = scrape_functions
+        self.scrape_classes = scrape_classes
+        self.scrape_primitives = scrape_primitives
+        self.primitives_list = primitives_list or (int, float, bool, np.ndarray)
 
-    def activate(self, safe=True, recursive=False):
+    def activate(self):
         """
         Parse the module assigned at instantiation, using decorator information assigned at instantiation.
-
-        Args:
-            safe (bool): Whether to skip values beginning with an underscore. (Default is True, do skip.)
-            recursive (bool): Whether to recursively activate submodules. (Default is False: submodules are noted by
-                creating a new `ModuleScraper` instance and setting it as an attribute, but contents cannot be accessed
-                until the submodule itself is explicitly activated.)
         """
         if inspect.ismodule(self._module):
             module = self._module
@@ -69,25 +93,34 @@ class ModuleScraper:
             module = import_module(self._module)
 
         for name, obj in inspect.getmembers(module):
-            if safe and name[0] == '_':
+            if self.safe and name[0] == '_':
                 continue
-
-            primitives = (int, float, bool, np.ndarray)
-            if self._scrape_methods and inspect.isfunction(obj):
+            elif self.recursive and inspect.ismodule(obj) and obj.__package__ == module.__package__:
+                # Behave recursively for submodules
+                submodule = ModuleScraper(
+                    obj,
+                    decorator=self._decorator,
+                    decorator_args=self._decorator_args,
+                    safe=self.safe,
+                    recursive=self.recursive,
+                    scrape_functions=self.scrape_functions,
+                    scrape_classes=self.scrape_classes,
+                    scrape_primitives=self.scrape_primitives,
+                    primitives_list=self.primitives_list
+                )
+                setattr(self, name, submodule)
+                if self.recursive:
+                    submodule.activate()
+            elif self.scrape_functions and inspect.isfunction(obj):
                 # Set all module functions as methods
                 if self._decorator is not None:
                     fnc = self._decorator(*self._decorator_args)(obj)
                 else:
                     fnc = obj
                 setattr(self, name, fnc)
-            elif recursive and inspect.ismodule(obj) and obj.__package__ == module.__package__:
-                # Behave recursively for submodules
-                submodule = ModuleScraper(obj, decorator=self._decorator, decorator_args=self._decorator_args)
-                setattr(self, name, submodule)
-                if recursive:
-                    submodule.activate(safe=safe, recursive=True)
-            elif inspect.isclass(obj) or isinstance(obj, primitives):
-                # Set all classes and all primitives (of the pre-registered types) as attributes
+            elif self.scrape_classes and inspect.isclass(obj):
+                setattr(self, name, obj)
+            elif self.scrape_primitives and isinstance(obj, self.primitives_list):
                 setattr(self, name, obj)
 
     def __getattr__(self, item):

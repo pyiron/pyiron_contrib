2b7d35820997123a194db579cb456b24c081f321
Jan Janßen <janssen@mpie.de>
2020-10-09 15:55:00 +0200

    Setup & Installation
    In preparation to migrate parts of pyiron_mpie to pyiron_contrib we should implement the same setup scripts we use for pyiron.

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyiron_contrib',
+    version='0.0.1',
+    description='Repository for the pyiron plugins developed at the Max Planck Institute für Eisenforschung.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_contrib',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='huber@mpie.de',
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*"]),
+    install_requires=[
+        'ase',
+        'matplotlib',
+        'numpy',
+        'pyiron',
+        'scipy',
+        'seaborn',
+        'skimage',
+    ]
+)

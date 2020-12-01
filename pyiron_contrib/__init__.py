0d1aae5f2c6f6ab522aceec656318920b8ddb0e8
Raynol Dsouza <dsouza@mpie.de>
2020-12-01 18:50:39 +0100

    Re-add HarmonicMD
    The HessianJob calculates the restoring forces for static MD positions, and needs to run in a loop over MD snapshots of the atom positions. This protocol is independent of the MD run that the HessianJob needs, and uses Langevin Dynamics independent of a Calculator (Lammps, VASP etc.). I will leave this here, as it this protocol presently is faster than running HessianJob over a loop.

--- pyiron_contrib/__init__.py
+++ pyiron_contrib/__init__.py
@@ -8,6 +8,7 @@ from pyiron_base import JOB_CLASS_DICT
 JOB_CLASS_DICT['ProtocolMinimize'] = 'pyiron_contrib.protocol.compound.minimize'
 JOB_CLASS_DICT['ProtocolMD'] = 'pyiron_contrib.protocol.compound.molecular_dynamics'
 JOB_CLASS_DICT['ProtocolConfinedMD'] = 'pyiron_contrib.protocol.compound.molecular_dynamics'
+JOB_CLASS_DICT['ProtocolHarmonicMD'] = 'pyiron_contrib.protocol.compound.molecular_dynamics'
 JOB_CLASS_DICT['ProtocolNEB'] = 'pyiron_contrib.protocol.compound.nudged_elastic_band'
 JOB_CLASS_DICT['ProtocolQMMM'] = 'pyiron_contrib.protocol.compound.qmmm'
 JOB_CLASS_DICT['ProtocolHarmonicTILD'] = 'pyiron_contrib.protocol.compound.thermodynamic_integration'

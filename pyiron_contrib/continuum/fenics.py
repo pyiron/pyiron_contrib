45aa7105e8df58d7f5ffd5b6deacbb2339455984
Liam Huber <liam.huber@gmail.com>
2020-12-11 11:10:36 +0100

    Directly expose underlying libraries by their regular names

--- pyiron_contrib/continuum/fenics.py
+++ pyiron_contrib/continuum/fenics.py
@@ -6,7 +6,7 @@
 A job class for performing finite element simulations using the [FEniCS](https://fenicsproject.org) code.
 """
 
-import fenics as FEN
+import fenics
 import mshr
 from pyiron_base import GenericJob, InputList, PyironFactory
 from os.path import join
@@ -53,13 +53,21 @@ class Fenics(GenericJob):
         self._v = None  # the test function
         self._vtk_filename = join(self.project_hdf5.path, 'output.pvd')
 
+    @property
+    def fenics(self):
+        return fenics
+
+    @property
+    def mshr(self):
+        return mshr
+
     def generate_mesh(self):
         self._mesh = mshr.generate_mesh(self.domain, self.input.mesh_resolution)
-        self._V = FEN.FunctionSpace(self.mesh, self.input.element_type, self.input.element_order)
+        self._V = fenics.FunctionSpace(self.mesh, self.input.element_type, self.input.element_order)
         # TODO: Allow changing what type of function space is used (VectorFunctionSpace, MultiMeshFunctionSpace...)
         # TODO: Allow having multiple sets of spaces and test/trial functions
-        self._u = FEN.TrialFunction(self.V)
-        self._v = FEN.TestFunction(self.V)
+        self._u = fenics.TrialFunction(self.V)
+        self._v = fenics.TestFunction(self.V)
 
     def refresh(self):
         self.generate_mesh()
@@ -93,7 +101,7 @@ class Fenics(GenericJob):
         """
         Returns the gradient of the given argument.
         """
-        return FEN.grad(arg)
+        return fenics.grad(arg)
 
     def Constant(self, value):
         """
@@ -105,7 +113,7 @@ class Fenics(GenericJob):
         Returns:
             fenics.Constant: The wrapped value.
         """
-        return FEN.Constant(value)
+        return fenics.Constant(value)
 
     def DirichletBC(self, expression, boundary):
         """
@@ -115,23 +123,23 @@ class Fenics(GenericJob):
             expression (string): The expression used to evaluate the value of the unknown on the boundary.
             boundary (fenics.DirichletBC): The spatial boundary, which the condition will be applied to.
         """
-        return FEN.DirichletBC(self.V, expression, boundary)
+        return fenics.DirichletBC(self.V, expression, boundary)
     
     def dot(self, arg1, arg2):
         """
         Returns the dot product between the FEniCS objects.
         """
-        return FEN.dot(arg1, arg2)
+        return fenics.dot(arg1, arg2)
 
     @property
     def dx(self):
         """
         Returns the FEniCS dx object.
         """
-        return FEN.dx
+        return fenics.dx
 
     def Expression(self, *args, **kwargs):
-        return FEN.Expression(*args, **kwargs)
+        return fenics.Expression(*args, **kwargs)
 
     def BC_default(self, x, on_boundary):
         """
@@ -143,7 +151,7 @@ class Fenics(GenericJob):
         """
         Write the output to a .vtk file.
         """
-        vtkfile = FEN.File(self._vtk_filename)
+        vtkfile = fenics.File(self._vtk_filename)
         vtkfile << self.u
 
     def validate_ready_to_run(self):
@@ -164,8 +172,8 @@ class Fenics(GenericJob):
         unknown and RHS is the known part.
         """
         self.status.running = True
-        self._u = FEN.Function(self.V)
-        FEN.solve(self.LHS == self.RHS, self.u, self.BC)
+        self._u = fenics.Function(self.V)
+        fenics.solve(self.LHS == self.RHS, self.u, self.BC)
         self.status.collect = True
         self.run()
 
@@ -179,13 +187,13 @@ class Fenics(GenericJob):
         """
         Plots the unknown u.
         """
-        FEN.plot(self.u)
+        fenics.plot(self.u)
 
     def plot_mesh(self):
         """
         Plots the mesh.
         """
-        FEN.plot(self.mesh)
+        fenics.plot(self.mesh)
 
     def to_hdf(self, hdf=None, group_name=None):
         super().to_hdf(hdf=hdf, group_name=group_name)
@@ -211,7 +219,7 @@ class Creator:
 class DomainFactory(PyironFactory):
 
     def circle(self, center, radius):
-        return mshr.Circle(FEN.Point(*center), radius)
+        return mshr.Circle(fenics.Point(*center), radius)
     circle.__doc__ = mshr.Circle.__doc__
 
     def square(self, length, origin=None):
@@ -219,7 +227,7 @@ class DomainFactory(PyironFactory):
             x, y = 0, 0
         else:
             x, y = origin[0], origin[1]
-        return mshr.Rectangle(FEN.Point(0 + x, 0 + y), FEN.Point(length + x, length + y))
+        return mshr.Rectangle(fenics.Point(0 + x, 0 + y), fenics.Point(length + x, length + y))
     square.__doc__ = mshr.Rectangle.__doc__
 
     def __call__(self):

57f9dad9d00abb84638f223fac9be54899bbd52b
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-01-13 16:35:10 +0100

    :fire: Disable HDF storage on the project level

--- pyiron_contrib/project/project.py
+++ pyiron_contrib/project/project.py
@@ -20,11 +20,7 @@ class Project(ProjectCore):
                          sql_query=sql_query,
                          default_working_directory=default_working_directory
                          )
-        self._project_info = InputList(table_name="projectinfo")
-        self._metadata = InputList(table_name="metadata")
         self._project_browser = None
-        self.hdf5 = self.create_hdf(self.path, self.base_name + "_projectdata")
-        self.from_hdf()
     __init__.__doc__ = ProjectCore.__init__.__doc__
 
     @property
@@ -44,38 +40,6 @@ class Project(ProjectCore):
             self._project_browser.update(Vbox=Vbox, show_files=show_files)
         return self._project_browser.gui()
 
-    @property
-    def metadata(self):
-        return self._metadata
-
-    @metadata.setter
-    def metadata(self, metadata):
-        self._metadata = InputList(metadata, table_name="metadata")
-
-    @property
-    def project_info(self):
-        return self._project_info
-
-    @project_info.setter
-    def project_info(self, project_info):
-        self._project_info = InputList(project_info, table_name="projectinfo")
-
-    def to_hdf(self, hdf=None, group_name=None):
-        """Store meta data and info of the project in the project hdf5 file."""
-        if hdf is None:
-            hdf = self.hdf5
-        self._metadata.to_hdf(hdf, group_name=None)
-        self._project_info.to_hdf(hdf, group_name=None)
-
-    def from_hdf(self, hdf=None, group_name=None):
-        """Load meta data and info of the project from the project hdf5 file."""
-        if hdf is None:
-            hdf = self.hdf5
-        try:
-            self._metadata.from_hdf(hdf, group_name=None)
-            self._project_info.from_hdf(hdf, group_name=None)
-        except ValueError:
-            pass
 
     def copy(self):
         """
@@ -85,14 +49,6 @@ class Project(ProjectCore):
             Project: copy of the project object
         """
         new = Project(path=self.path, user=self.user, sql_query=self.sql_query)
-        return new
-
-    def open(self, rel_path, history=True):
-        new = super().open(rel_path, history=history)
-        new.hdf5 = new.create_hdf(new.path, new.base_name + "_projectdata")
-        new._metadata = InputList(table_name="metadata")
-        new._project_info = InputList(table_name="projectinfo")
-        new.from_hdf()
         return new
 
     def display_item(self, item, outwidget=None):

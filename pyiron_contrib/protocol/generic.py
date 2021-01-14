a5f148cc12886f95f4a9336cd1ef6c30fb586368
Raynol Dsouza <dsouza@mpie.de>
2021-01-14 12:00:44 +0100

    Remove redundant line

--- pyiron_contrib/protocol/generic.py
+++ pyiron_contrib/protocol/generic.py
@@ -668,7 +668,6 @@ class Protocol(CompoundVertex, GenericJob):
         """
         if hdf is None:
             hdf = self.project_hdf5
-        self.graph.to_hdf(hdf=hdf, group_name="graph")
         super(CompoundVertex, self).to_hdf(hdf=hdf, group_name=group_name)
 
     def from_hdf(self, hdf=None, group_name=None):

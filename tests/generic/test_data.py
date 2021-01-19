5b2ebce7a97e20c48d1cf8ff0dc906abebd6e863
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-01-19 18:04:25 +0100

    Adapt test.

--- tests/generic/test_data.py
+++ tests/generic/test_data.py
@@ -1,44 +1,42 @@
 import unittest
 
-from pyiron_contrib.generic.data import Data
+from pyiron_contrib.generic.filedata import FileData
 
 class TestData(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.data = Data(source="../static/test_data.txt")
+        cls.data = FileData(source="../static/test_data.txt")
 
     def test___init__(self):
         """Test init of data class"""
         # Test if init from setUpClass is as expected
-        self.assertFalse(self.data.hasdata)
+        self.assertFalse(self.data._hasdata)
         self.assertEqual(self.data.filename, "test_data.txt")
         self.assertEqual(self.data.filetype, "txt")
-        self.assertRaises(ValueError, Data())
-        data = Data(source="../static/test_data.txt", metadata={"some": "dict"})
-        self.assertFalse(data.hasdata)
+        self.assertRaises(ValueError, FileData())
+        data = FileData(source="../static/test_data.txt", metadata={"some": "dict"})
+        self.assertFalse(data._hasdata)
         self.assertEqual(data.filename, "test_data.txt")
         self.assertEqual(data.filetype, "txt")
         self.assertEqual(data.metadata["some"], "dict")
-        data = Data(source="../static/test_data.txt", storedata=True)
-        self.assertTrue(data.hasdata)
         with open("../static/test_data.txt") as f:
             some_data = f.readlines()
-        self.assertRaises(ValueError, Data(data=some_data))
-        data = Data(data=some_data, filename="test_data.dat")
-        self.assertTrue(data.hasdata)
+        self.assertRaises(ValueError, FileData(data=some_data))
+        data = FileData(data=some_data, filename="test_data.dat")
+        self.assertTrue(data._hasdata)
         self.assertEqual(data.filetype, "dat")
-        data = Data(data=some_data, filename="test_data.dat", filetype="txt")
+        data = FileData(data=some_data, filename="test_data.dat", filetype="txt")
         self.assertEqual(data.filetype, "txt")
 
     def test_data(self):
-        """Test data property of Data"""
+        """Test data property of FileData"""
         with open("../static/test_data.txt", "rb") as f:
-            some_data = f.read()
+            some_data = f.readlines()
         self.assertEqual(self.data.data, some_data)
 
     def test_data_as_numpy_array(self):
         """Test data conversion to numpy"""
-        self.assertTrue(self.data.data_as_np_array() is None)
+        self.assertTrue(self.data.data_as_np_array()[0] == 'some text')
 
 
 

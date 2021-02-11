8c25ad0d21d48eb1b0146a0f3602369cbe065f0e
Niklas Leimeroth <leimeroth@mm.tu-darmstadt.de>
2021-02-11 13:32:31 +0100

    Documentation output

--- pyiron_contrib/atomistic/atomicrex/output.py
+++ pyiron_contrib/atomistic/atomicrex/output.py
@@ -1,6 +1,11 @@
 from pyiron_base import InputList
 
 class Output(InputList):
+    """
+    Class to store general output quantities.
+    Final properties and function parameter values are stored within
+    the respective classes and not here.
+    """    
     def __init__(self):
         super().__init__(table_name="output")
         self.error = None
@@ -9,6 +14,20 @@ class Output(InputList):
 
     @staticmethod
     def _get_structure_lines(l_index_complete, lines):
+        """Internal function.
+        Filters the lines from atomicrex output
+        corresponding to structures and their properties.
+
+        Args:
+            l_index_complete (int): "Fitting process complete." index
+            lines (list[str]): atomicrex output
+
+        Raises:
+            RuntimeError: Raises if the formatting is not as expected.
+
+        Returns:
+            [list[str]]: lines containing structure information
+        """
         end_index = None
         start_index = l_index_complete+3
         if lines[start_index-1] != "Computing structure properties\n":
@@ -22,6 +41,17 @@ class Output(InputList):
 
     @staticmethod
     def _get_parameter_lines(l_index_params, lines):
+        """Internal Function
+        Filters atomicrex output to find lines corresponding
+        to final function parameters.
+
+        Args:
+            l_index_params (int): index at which parameter start in output
+            lines (list[str]): atomicrex output
+
+        Returns:
+            [type]: [description]
+        """        
         # Get number of fitted parameters from a line similar to:
         # Potential parameters being optimized (#dof=18):
         n_params = int(lines[l_index_params].split("=")[-1].split(")")[0])

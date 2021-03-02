7a330fa1be95e6d9eab59d93a25b996fc5ec66af
sudarsan1989 <surendralal@mpie.de>
2021-03-02 14:56:42 +0100

    :white_check_mark: Including tests

---
+++ tests/atomistics/job/test_trainingcontainer.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+import os
+import unittest
+from pyiron import Project
+import pyiron_contrib
+print(pyiron_contrib.__file__)
+
+
+class TestTrainingContainer(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.execution_path = os.path.dirname(os.path.abspath(__file__))
+        cls.project = Project(os.path.join(cls.execution_path, "test_training"))
+        cls.container = cls.project.create.job.TrainingContainer("test")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.execution_path = os.path.dirname(os.path.abspath(__file__))
+        project = Project(os.path.join(cls.execution_path, "test_training"))
+        project.remove_jobs_silently(recursive=True)
+        project.remove(enable=True)
+
+    def test_include_structure(self):
+        basis_1 = self.project.create_ase_bulk("Al")
+        force = [[0.0, 0.0, 0.0]]
+        energy = 0.0
+        self.container.include_structure(basis_1, energy=energy, forces=force, name="unitcell")
+        basis_2 = self.project.create_ase_bulk("Al").repeat([2, 1, 1])
+        force = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.01]]
+        energy = 0.01
+        self.container.include_structure(basis_2, energy=energy, forces=force, name="repeated")
+        self.container.run()
+        structure_list, energy_list, forces_list, num_atoms = self.container.get_data_list()
+        self.assertEqual(len(structure_list), 2)
+        self.assertEqual(structure_list[0], basis_1)
+        self.assertEqual(structure_list[1], basis_2)
+        self.assertEqual(num_atoms, [1, 2])
+        self.assertEqual(energy_list, [0.0, 0.01])
+        structure_list, energy_list, _, _ = self.container.get_data_list(filter_function=
+                                                                         lambda df: df[df.number_of_atoms > 1])
+        self.assertEqual(len(structure_list), 1)
+        self.assertEqual(energy_list[0], 0.01)

984783eff4cb55acde9748b9c11bd29205e6c604
Marvin Poul <ponder@creshal.de>
2021-03-03 11:13:40 +0100

    Fix indices

--- pyiron_contrib/atomistic/runner/job.py
+++ pyiron_contrib/atomistic/runner/job.py
@@ -88,9 +88,9 @@ class RunnerFit(GenericJob):
                     p = atoms.positions * AngstromToBohr
                     ff = forces * ElectronVoltToHartree / AngstromToBohr
                     for i in range(len(atoms)):
-                        f.write(f"atom {p[i, 0]} {p[i, 0]} {p[i, 0]}")
+                        f.write(f"atom {p[i, 0]} {p[i, 1]} {p[i, 1]}")
                         f.write(f" {atoms.elements[i].Abbreviation} 0.0 0.0")
-                        f.write(f" {ff[i, 0]} {ff[i, 0]} {ff[i, 0]}\n")
+                        f.write(f" {ff[i, 0]} {ff[i, 1]} {ff[i, 2]}\n")
                     f.write(f"energy {energy * ElectronVoltToHartree}\n")
                     f.write("charge 0.0\nend\n")
 

ed235ec4ddcbd484caf30f21b9235409617662d2
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-12-10 11:28:13 -0700

    Update __init__.py

--- pyiron_contrib/__init__.py
+++ pyiron_contrib/__init__.py
@@ -44,7 +44,7 @@ JOB_CLASS_DICT['MlipJob'] = 'pyiron_contrib.atomistics.mlip.mlipjob'
 JOB_CLASS_DICT['Atomicrex'] = 'pyiron_contrib.atomistics.atomicrex.atomicrex_job'
 JOB_CLASS_DICT['StructureMasterInt'] = 'pyiron_contrib.atomistics.atomistics.job.structurelistmasterinteractive'
 JOB_CLASS_DICT['StorageJob'] = 'pyiron_contrib.RDM.storagejob'
-JOB_CLASS_DICT['QuantumEspresso'] = 'pyiron_contrib.quantumespresso.asejob'
+JOB_CLASS_DICT['QuantumEspresso'] = 'pyiron_contrib.atomistics.quantumespresso.asejob'
 
 from ._version import get_versions
 

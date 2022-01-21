93ccd9b3ad9b6646637bd5c7cd88971950b44768
Niklas Leimeroth <leimeroth@mm.tu-darmstadt.de>
2022-01-21 12:21:54 +0100

    TrainingStorage

--- pyiron_contrib/atomistics/atomistics/job/trainingcontainer.py
+++ pyiron_contrib/atomistics/atomistics/job/trainingcontainer.py
@@ -48,30 +48,11 @@ class TrainingContainer(GenericJob, HasStructure):
         super().__init__(project=project, job_name=job_name)
         self.__name__ = "TrainingContainer"
         self.__hdf_version__ = "0.2.0"
-        self._container = StructureStorage()
-        self._container.add_array("energy", dtype=np.float64, per="chunk")
-        self._container.add_array("forces", shape=(3,), dtype=np.float64, per="element")
-        # save stress in voigt notation
-        self._container.add_array("stress", shape=(6,), dtype=np.float64, per="chunk")
-        self._table_cache = None
+        self._container = TrainingStorage()
 
     @property
     def _table(self):
-        if self._table_cache is None or len(self._table_cache) != len(self._container):
-            self._table_cache = pd.DataFrame({
-                "name":             [self._container.get_array("identifier", i)
-                                        for i in range(len(self._container))],
-                "atoms":            [self._container.get_structure(i)
-                                        for i in range(len(self._container))],
-                "energy":           [self._container.get_array("energy", i)
-                                        for i in range(len(self._container))],
-                "forces":           [self._container.get_array("forces", i)
-                                        for i in range(len(self._container))],
-                "stress":           [self._container.get_array("stress", i)
-                                        for i in range(len(self._container))],
-            })
-            self._table_cache["number_of_atoms"] = [len(s) for s in self._table_cache.atoms]
-        return self._table_cache
+        return self._container._table_cache
 
     def include_job(self, job, iteration_step=-1):
         """
@@ -81,29 +62,7 @@ class TrainingContainer(GenericJob, HasStructure):
             job (:class:`.AtomisticGenericJob`): job to take structure from
             iteration_step (int, optional): if job has multiple steps, this selects which to add
         """
-        energy = job.output.energy_pot[iteration_step]
-        ff = job.output.forces
-        if ff is not None:
-            forces = ff[iteration_step]
-        else:
-            forces = None
-        # HACK: VASP work-around, current contents of pressures are meaningless, correct values are in
-        # output/generic/stresses
-        pp = job["output/generic/stresses"]
-        if pp is None:
-            pp = job.output.pressures
-        if pp is not None:
-            stress = pp[iteration_step]
-        else:
-            stress = None
-        if stress is not None:
-            stress = np.asarray(stress)
-            if stress.shape == (3, 3):
-                stress = np.array([stress[0, 0], stress[1 ,1], stress[2, 2],
-                                   stress[1, 2], stress[0, 2], stress[0, 1]])
-        self.include_structure(job.get_structure(iteration_step=iteration_step),
-                               energy=energy, forces=forces, stress=stress,
-                               name=job.name)
+        self._container.include_job(job, iteration_step)
 
     def include_structure(self, structure, energy, forces=None, stress=None, name=None):
         """
@@ -119,17 +78,7 @@ class TrainingContainer(GenericJob, HasStructure):
             stress (6 array of float, optional): per structure stresses in voigt notation
             name (str, optional): name describing the structure
         """
-        data = {"energy": energy}
-        if forces is not None:
-            data["forces"] = forces
-        if stress is not None:
-            data["stress"] = stress
-        self._container.add_structure(structure, name, **data)
-        if self._table_cache:
-            self._table = self._table.append(
-                    {"name": name, "atoms": structure, "energy": energy, "forces": forces, "stress": stress,
-                     "number_of_atoms": len(structure)},
-                    ignore_index=True)
+        self._container.include_structure(structure, energy, forces, stress, name)
 
     def include_dataset(self, dataset):
         """
@@ -142,16 +91,7 @@ class TrainingContainer(GenericJob, HasStructure):
             - forces (Nx3 array of float): per atom forces, where N is the number of atoms in the structure
             - stress (6 array of float): per structure stress in voigt notation
         """
-        self._table_cache = self._table.append(dataset, ignore_index=True)
-        # in case given dataset has more columns than the necessary ones, swallow/ignore them in *_
-        for name, atoms, energy, forces, stress, *_ in dataset.itertuples(index=False):
-            self._container.add_structure(atoms, name, energy=energy, forces=forces, stress=stress)
-
-    def _get_structure(self, frame=-1, wrap_atoms=True):
-        return self._container.get_structure(frame=frame, wrap_atoms=wrap_atoms)
-
-    def _number_of_structures(self):
-        return self._container.number_of_structures
+        self._container.include_dataset(dataset)
 
     def get_elements(self):
         """
@@ -189,15 +129,7 @@ class TrainingContainer(GenericJob, HasStructure):
         Returns:
             tuple: list of structures, energies, forces, and the number of atoms
         """
-        if filter_function is None:
-            data_table = self._table
-        else:
-            data_table = filter_function(self._table)
-        structure_list = data_table.atoms.to_list()
-        energy_list = data_table.energy.to_list()
-        force_list = data_table.forces.to_list()
-        num_atoms_list = data_table.number_of_atoms.to_list()
-        return structure_list, energy_list, force_list, num_atoms_list
+        return self._container.to_list(filter_function)
 
     def write_input(self):
         pass
@@ -223,7 +155,7 @@ class TrainingContainer(GenericJob, HasStructure):
             table = pd.read_hdf(self.project_hdf5.file_name, self.name + "/output/structure_table")
             self.include_dataset(table)
         else:
-            self._container = StructureStorage()
+            self._container = TrainingStorage()
             self._container.from_hdf(self.project_hdf5, "structures")
 
     @property
@@ -380,3 +312,124 @@ class TrainingPlots:
         plt.ylabel(r"Atomic Energy [eV]")
 
         return pd.DataFrame({"V": V, "E": E})
+
+class TrainingStorage(StructureStorage):
+    def __init__(self):
+        super().__init__()
+        self.add_array("energy", dtype=np.float64, per="chunk", fill=np.nan)
+        self.add_array("forces", shape=(3,), dtype=np.float64, per="element", fill=np.nan)
+        # save stress in voigt notation
+        self.add_array("stress", shape=(6,), dtype=np.float64, per="chunk", fill=np.nan)
+        self._table_cache = None
+
+    @property
+    def _table(self):
+        if self._table_cache is None or len(self._table_cache) != len(self):
+            self._table_cache = pd.DataFrame({
+                "name":             [self.get_array("identifier", i)
+                                        for i in range(len(self))],
+                "atoms":            [self.get_structure(i)
+                                        for i in range(len(self))],
+                "energy":           [self.get_array("energy", i)
+                                        for i in range(len(self))],
+                "forces":           [self.get_array("forces", i)
+                                        for i in range(len(self))],
+                "stress":           [self.get_array("stress", i)
+                                        for i in range(len(self))],
+            })
+            self._table_cache["number_of_atoms"] = [len(s) for s in self._table_cache.atoms]
+        return self._table_cache
+
+    def include_job(self, job, iteration_step=-1):
+        """
+        Add structure, energy and forces from job.
+
+        Args:
+            job (:class:`.AtomisticGenericJob`): job to take structure from
+            iteration_step (int, optional): if job has multiple steps, this selects which to add
+        """
+        energy = job.output.energy_pot[iteration_step]
+        ff = job.output.forces
+        if ff is not None:
+            forces = ff[iteration_step]
+        else:
+            forces = None
+        # HACK: VASP work-around, current contents of pressures are meaningless, correct values are in
+        # output/generic/stresses
+        pp = job["output/generic/stresses"]
+        if pp is None:
+            pp = job.output.pressures
+        if pp is not None:
+            stress = pp[iteration_step]
+        else:
+            stress = None
+        if stress is not None:
+            stress = np.asarray(stress)
+            if stress.shape == (3, 3):
+                stress = np.array([stress[0, 0], stress[1 ,1], stress[2, 2],
+                                   stress[1, 2], stress[0, 2], stress[0, 1]])
+        self.include_structure(job.get_structure(iteration_step=iteration_step),
+                               energy=energy, forces=forces, stress=stress,
+                               name=job.name)
+
+    def include_structure(self, structure, energy, forces=None, stress=None, name=None):
+        """
+        Add new structure to structure list and save energy and forces with it.
+
+        For consistency with the rest of pyiron, energy should be in units of eV and forces in eV/A, but no conversion
+        is performed.
+
+        Args:
+            structure_or_job (:class:`~.Atoms`): structure to add
+            energy (float): energy of the whole structure
+            forces (Nx3 array of float, optional): per atom forces, where N is the number of atoms in the structure
+            stress (6 array of float, optional): per structure stresses in voigt notation
+            name (str, optional): name describing the structure
+        """
+        data = {"energy": energy}
+        if forces is not None:
+            data["forces"] = forces
+        if stress is not None:
+            data["stress"] = stress
+        self.add_structure(structure, name, **data)
+        if self._table_cache:
+            self._table = self._table.append(
+                    {"name": name, "atoms": structure, "energy": energy, "forces": forces, "stress": stress,
+                     "number_of_atoms": len(structure)},
+                    ignore_index=True)
+
+    def include_dataset(self, dataset):
+        """
+        Add a pandas DataFrame to the saved structures.
+
+        The dataframe should have the following columns:
+            - name: human readable name of the structure
+            - atoms(:class:`ase.Atoms`): the atomic structure
+            - energy(float): energy of the whole structure
+            - forces (Nx3 array of float): per atom forces, where N is the number of atoms in the structure
+            - stress (6 array of float): per structure stress in voigt notation
+        """
+        self._table_cache = self._table.append(dataset, ignore_index=True)
+        # in case given dataset has more columns than the necessary ones, swallow/ignore them in *_
+        for name, atoms, energy, forces, stress, *_ in dataset.itertuples(index=False):
+            self.add_structure(atoms, name, energy=energy, forces=forces, stress=stress)
+
+    def to_list(self, filter_function=None):
+        """
+        Returns the data as lists of pyiron structures, energies, forces, and the number of atoms
+
+        Args:
+            filter_function (function): Function applied to the dataset (which is a pandas DataFrame) to filter it
+
+        Returns:
+            tuple: list of structures, energies, forces, and the number of atoms
+        """
+        if filter_function is None:
+            data_table = self._table
+        else:
+            data_table = filter_function(self._table)
+        structure_list = data_table.atoms.to_list()
+        energy_list = data_table.energy.to_list()
+        force_list = data_table.forces.to_list()
+        num_atoms_list = data_table.number_of_atoms.to_list()
+        return structure_list, energy_list, force_list, num_atoms_list

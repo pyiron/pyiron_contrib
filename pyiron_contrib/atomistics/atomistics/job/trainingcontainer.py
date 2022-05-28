74e850d51a2ba8781d4ab71b90f11800f943de98
Niklas Leimeroth <leimeroth@mm.tu-darmstadt.de>
2022-05-28 08:43:14 +0200

    indent

--- pyiron_contrib/atomistics/atomistics/job/trainingcontainer.py
+++ pyiron_contrib/atomistics/atomistics/job/trainingcontainer.py
@@ -690,7 +690,7 @@ class TrainingStorage(StructureStorage):
         Yields:
             :class:`pyiron_atomistics.atomistitcs.structure.atoms.Atoms`, arrays: every structure attached to the object and queried arrays
         """
-yield from zip(self._container.iter_structures(), *(self._container.get_array_ragged(a) for a in arrays))
+        yield from zip(self._container.iter_structures(), *(self._container.get_array_ragged(a) for a in arrays))
 
     @property
     def plot(self):

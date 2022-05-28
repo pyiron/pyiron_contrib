ac0028e7dd2fbd59ed62cc5cd7b0a7abe8967b44
Niklas Leimeroth <leimeroth@mm.tu-darmstadt.de>
2022-05-28 08:45:42 +0200

    safe some function calls

--- pyiron_contrib/atomistics/atomistics/job/trainingcontainer.py
+++ pyiron_contrib/atomistics/atomistics/job/trainingcontainer.py
@@ -690,7 +690,8 @@ class TrainingStorage(StructureStorage):
         Yields:
             :class:`pyiron_atomistics.atomistitcs.structure.atoms.Atoms`, arrays: every structure attached to the object and queried arrays
         """
-        yield from zip(self._container.iter_structures(), *(self._container.get_array_ragged(a) for a in arrays))
+        array_vals = (self._container.get_array_ragged(a) for a in arrays)
+        yield from zip(self._container.iter_structures(), *array_vals)
 
     @property
     def plot(self):

d231804520a85d4c63954c34420e3cfb16a4454d
Jan Janssen <jan-janssen@users.noreply.github.com>
2022-07-22 06:49:57 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[        
         'matplotlib==3.5.2',
         'numpy==1.23.1',
-        'pyiron_base==0.5.17',
+        'pyiron_base==0.5.19',
         'scipy==1.8.1',
         'seaborn==0.11.2',
         'pyparsing==3.0.9'

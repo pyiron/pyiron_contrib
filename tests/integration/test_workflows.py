2100b7f367997d44ca5e71333d2059bf191478a8
liamhuber <liamhuber@greyhavensolutions.com>
2023-02-02 14:42:47 -0800

    Just use project

--- tests/integration/test_workflows.py
+++ tests/integration/test_workflows.py
@@ -1,21 +1,22 @@
 
 from unittest import TestCase
-from pyiron_contrib.workflow import Session
+from pyiron_contrib import Project
 
 
 class TestWorkflows(TestCase):
     def test_workflows(self):
 
-        s = Session("dir_if_theres_storage")
-        wf = s.create.workflow("subdir_if_theres_storage")
+        pr = Project("dir_if_theres_storage")
+        wf = pr.create.workflow("subdir_if_theres_storage")
+        nodes = pr.create.node
 
-        wf.structure = s.nodes.atomistics.BulkStructure(element='Ni',
+        wf.structure = nodes.atomistics.BulkStructure(element='Ni',
                                                         cubic=True,
                                                         repeat=3)
 
-        wf.engine = s.nodes.atomistics.Lammps(structure=wf.structure.output.structure)
+        wf.engine = nodes.atomistics.Lammps(structure=wf.structure.output.structure)
 
-        wf.calc_md = s.nodes.atomistics.CalcMD(temperature=300,
+        wf.calc_md = nodes.atomistics.CalcMD(temperature=300,
                                                n_steps=1000,
                                                n_print=100,
                                                name="my_MD_run")
@@ -24,17 +25,17 @@ class TestWorkflows(TestCase):
         wf.calc_md.output.steps.store()  # and file storage of output is default blacklisted
         wf.calc_md.output.temperature.store()  # so we turn on saving a couple things here
 
-        wf.pressure = s.nodes.atomistics.Pressure(voigt=(1, 2, 3, 0, 0, 0))
+        wf.pressure = nodes.atomistics.Pressure(voigt=(1, 2, 3, 0, 0, 0))
 
-        wf.plot = s.nodes.base.Plot2d(x=wf.calc_md.output.steps,
+        wf.plot = nodes.base.Plot2d(x=wf.calc_md.output.steps,
                                       y=wf.calc_md.output.temperature)
         wf.plot.output.plot.value.show()
 
-        s.subdir_if_theres_storage.serialize()  # gets wf by name
+        pr["subdir_if_theres_storage"].serialize()  # gets wf by name
         # Saves graph topology, and whatever IO we flagged to serialize
         # I have no idea yet how database connections should be made
 
-        new_s = Session(s.name)
-        new_wf = new_s.create.workflow(wf.name)
+        new_pr = Project(pr.name)
+        new_wf = new_pr.create.workflow(wf.name)
         new_wf.deserialize()
         new_wf.plot.output.plot.value.show()

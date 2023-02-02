ed398382f8bd58408bad989cbf296d9934bfbf52
liamhuber <liamhuber@greyhavensolutions.com>
2023-02-02 10:53:18 -0800

    Aspire to the pseudo example from github

---
+++ tests/integration/test_workflows.py
@@ -0,0 +1,40 @@
+
+from unittest import TestCase
+from pyiron_contrib.workflow import Session
+
+
+class TestWorkflows(TestCase):
+    def test_workflows(self):
+
+        s = Session("dir_if_theres_storage")
+        wf = s.create.workflow("subdir_if_theres_storage")
+
+        wf.structure = s.nodes.atomistics.BulkStructure(element='Ni',
+                                                        cubic=True,
+                                                        repeat=3)
+
+        wf.engine = s.nodes.atomistics.Lammps(structure=wf.structure.output.structure)
+
+        wf.calc_md = s.nodes.atomistics.CalcMD(temperature=300,
+                                               n_steps=1000,
+                                               n_print=100,
+                                               name="my_MD_run")
+        wf.calc_md.input.job = wf.engine.output.job
+        # Maybe file storage of input is default whitelisted
+        wf.calc_md.output.steps.store()  # and file storage of output is default blacklisted
+        wf.calc_md.output.temperature.store()  # so we turn on saving a couple things here
+
+        wf.pressure = s.nodes.atomistics.Pressure(voigt=(1, 2, 3, 0, 0, 0))
+
+        wf.plot = s.nodes.base.Plot2d(x=wf.calc_md.output.steps,
+                                      y=wf.calc_md.output.temperature)
+        wf.plot.output.plot.value.show()
+
+        s.subdir_if_theres_storage.serialize()  # gets wf by name
+        # Saves graph topology, and whatever IO we flagged to serialize
+        # I have no idea yet how database connections should be made
+
+        new_s = Session(s.name)
+        new_wf = new_s.create.workflow(wf.name)
+        new_wf.deserialize()
+        new_wf.plot.output.plot.value.show()

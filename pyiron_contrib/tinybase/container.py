6ca106c26f9eaab316fcb60d3fc62bcb68358f6a
Marvin Poul <ponder@creshal.de>
2023-03-03 16:11:55 +0100

    Allow to manually override __module__ of dynamical input/output classes
    Otherwise the pickling won't work, because it thinks it's defined in
    abc.

--- pyiron_contrib/tinybase/container.py
+++ pyiron_contrib/tinybase/container.py
@@ -24,31 +24,36 @@ def make_storage_mapping(name, default=lambda: None):
 class AbstractContainer(HasStorage, abc.ABC):
     # TODO: this should go into HasStorage, exists here only to give one location to define from_ methods
     @classmethod
-    def from_attributes(cls, name, *attrs, **default_attrs):
+    def from_attributes(cls, name, *attrs, module=None, **default_attrs):
         """
         Create a new sub class with given attributes.
 
         Args:
             name (str): name of the new class
             *attrs (str): names of the new attributes
+            module (str, optional): the module path where this class is defined; you cannot pickle instances of classes
+            defined by this function, if you do not provide this as __name__
             **default_attrs (str): names and defaults of new attributes
         """
         body = {a: make_storage_mapping(a) for a in attrs}
         body.update({a: make_storage_mapping(a, d) for a, d in default_attrs.items()})
-        return type(name, (cls,), body)
+        T = type(name, (cls,), body)
+        T.__module__ = module
+        return T
 
 
 class AbstractInput(AbstractContainer, abc.ABC):
     pass
 
-StructureInput = AbstractInput.from_attributes("StructureInput", "structure")
+StructureInput = AbstractInput.from_attributes("StructureInput", "structure", module=__name__)
 
 MDInput = AbstractInput.from_attributes(
         "MDInput",
         "steps",
         "timestep",
         "temperature",
-        "output_steps"
+        "output_steps",
+        module=__name__
 )
 
 
@@ -57,7 +62,8 @@ class AbstractOutput(AbstractContainer, abc.ABC):
 
 EnergyOutput = AbstractOutput.from_attributes(
         "EnergyOutput",
-        "energy_pot"
+        "energy_pot",
+        module=__name__
 )
 
 MDOutputBase = AbstractOutput.from_attributes(

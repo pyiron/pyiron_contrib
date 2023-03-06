bf2821080ca527cfbebcb4ca36ec5834b5dab6ed
Marvin Poul <ponder@creshal.de>
2023-03-06 13:42:09 +0100

    Rewrite ListNode and add SeriesNode
    Both now take an additional argument, the executor class to use for
    their children.  This now allows complete freedom how nodes are run and
    still allows master nodes (ListNode and SeriesNode) to decide themselves
    how they want their children to be run, without depending on executor
    details.

--- pyiron_contrib/tinybase/__init__.py
+++ pyiron_contrib/tinybase/__init__.py
@@ -100,7 +100,105 @@ class FunctionNode(AbstractNode):
     def _execute(self):
         self.output.result = self._function(*self.input.args, **self.input.kwargs)
 
+MasterInput = AbstractInput.from_attributes(
+        "MasterInput",
+        child_executor=lambda: Executor
+)
+
+class ListInput(MasterInput, abc.ABC):
+
+    @abc.abstractmethod
+    def _create_nodes(self):
+        pass
+
 class ListNode(AbstractNode, abc.ABC):
+
+    def __init__(self):
+        super().__init__()
+
+    @abc.abstractmethod
+    def _extract_output(self, step, node, ret):
+        pass
+
+    def _execute(self):
+        nodes = self.input._create_nodes()
+        exe = self.input.child_executor(nodes)
+        exe.run()
+        exe.wait()
+
+        for i, (node, ret) in enumerate(zip(nodes, exe._run_machine._data["status"])):
+            self._extract_output(i, node, ret)
+
+SeriesInputBase = AbstractInput.from_attributes(
+        "SeriesInputBase",
+        nodes=list,
+        connections=list
+)
+
+class SeriesInput(SeriesInputBase, MasterInput):
+    def check_ready(self):
+        return len(self.nodes) == len(connections) + 1
+
+    def first(self, node):
+        """
+        Set initial node.
+
+        Resets whole input
+
+        Args:
+            node (AbstractNode): the first node to execute
+
+        Returns:
+            self: the input object
+        """
+        self.nodes = [node]
+        self.connections = []
+        return self
+
+    def then(self, next_node, connection):
+        """
+        Add a new node and how to connect it to the previous node.
+
+        Args:
+            next_node (:class:`~.AbstractNode`): next node to execute
+            connection (function): takes the input of next_node and the output of the previous node
+
+        Returns:
+            self: the input object
+        """
+        self.nodes.append(next_node)
+        self.connections.append(connection)
+        return self
+
+class SeriesNode(AbstractNode):
+
+    def _get_input(self):
+        return SeriesInput()
+
+    def _get_output(self):
+        return self.input.nodes[-1]._get_output()
+
+    def _execute(self):
+        Exe = self.input.child_executor
+
+        first = self.input.nodes[0]
+        exe = Exe([first])
+        exe.run()
+        exe.wait()
+
+        prev = first
+        for node, connection in zip(self.input.nodes[1:], self.input.connections):
+            connection(node.input, prev.output)
+            exe = Exe([node])
+            exe.run()
+            exe.wait()
+            ret = exe._run_machine._data["status"][0]
+            if ret is not None and not ret.is_done():
+                return ReturnStatus("aborted", ret)
+
+
+
+class TinyJob(abc.ABC):
 
     _executors = {
             'foreground': Executor,
@@ -108,36 +206,26 @@ class ListNode(AbstractNode, abc.ABC):
             'process': ProcessExecutor
     }
 
-    def __init__(self):
-        super().__init__()
-        self._nodes = None
+    def __init__(self, project, job_name):
+        self._project = project
+        self._name = job_name
 
     @abc.abstractmethod
-    def _create_nodes(self):
+    def _get_node(self):
         pass
 
     @property
-    def nodes(self):
-        if self._nodes is None:
-            self._nodes = self._create_nodes()
-        return self._nodes
+    def input(self):
+        return self._node.input
 
-    @abc.abstractmethod
-    def _extract_output(self, step, node, ret):
-        pass
+    @property
+    def output(self):
+        return self._node.output
 
-    # If our own execute is called we act like a normal node, executing child nodes and then process their output
-    def _execute(self):
-        for i, node in enumerate(self.nodes):
-            ret = node.execute()
-            self._extract_output(i, node, ret)
-
-    # If called via run by the user directly we can also dispatch to a list executor
     def run(self, how='foreground'):
-        exe = self._executors[how](self.nodes)
-        exe._run_machine.observe("collect",
-                lambda data: [self._extract_output(i, n, r)
-                                for i, (n, r) in enumerate(zip(self.nodes, data['status']))]
-        )
+        exe = self._executor = self._executors[how](nodes=[self._node])
+        exe._run_machine.observe("ready", self.save_input)
+        exe._run_machine.observe("collect", self.save_output)
         exe.run()
         return exe
+
--- pyiron_contrib/tinybase/murn.py
+++ pyiron_contrib/tinybase/murn.py
@@ -1,14 +1,18 @@
 from .container import (
+            AbstractOutput,
             StructureInput,
-            AbstractOutput
 )
-from . import AbstractNode, ListNode
+from . import AbstractNode, ListNode, ListInput
 from . import ReturnStatus
 
 from copy import deepcopy
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.interpolate as si
+import scipy.optimize as so
+
+from pyiron_atomistics.atomistics.structure.has_structure import HasStructure
 
 MurnaghanInputBase = StructureInput.from_attributes(
         "MurnaghanInputBase",
@@ -16,7 +20,7 @@ MurnaghanInputBase = StructureInput.from_attributes(
         "node"
 )
 
-class MurnaghanInput(MurnaghanInputBase):
+class MurnaghanInput(MurnaghanInputBase, ListInput):
     def check_ready(self):
         structure_ready = self.structure is not None
         strain_ready = len(self.strains) > 0
@@ -27,15 +31,39 @@ class MurnaghanInput(MurnaghanInputBase):
     def set_strain_range(self, range, steps):
         self.strains = (1 + np.linspace(-range, range, steps))**(1/3)
 
+    def _create_nodes(self):
+        cell = self.structure.get_cell()
+        nodes = []
+        for s in self.strains:
+            n = deepcopy(self.node)
+            n.input.structure = self.structure.copy()
+            n.input.structure.set_cell(cell * s, scale_atoms=True)
+            nodes.append(n)
+        return nodes
+
 MurnaghanOutputBase = AbstractOutput.from_attributes(
         "MurnaghanOutputBase",
+        'base_structure',
         volumes=list,
         energies=list
 )
 
-class MurnaghanOutput(MurnaghanOutputBase):
+class MurnaghanOutput(MurnaghanOutputBase, HasStructure):
     def plot(self):
         plt.plot(self.volumes, self.energies)
+
+    @property
+    def equilibrium_volume(self):
+        inter = si.interp1d(self.volumes, self.energies)
+        return so.minimize_scalar(inter, bounds=(np.min(self.volumes), np.max(self.volumes))).x
+
+    def _number_of_structures(self):
+        return 1
+
+    def _get_structure(self, frame, wrap_atoms=True):
+        s = self.base_structure
+        s.set_cell(s.get_cell() * (self.equilibrium_volume/s.get_volume())**(1/3))
+        return s
 
 class MurnaghanNode(ListNode):
 
@@ -45,15 +73,9 @@ class MurnaghanNode(ListNode):
     def _get_output(self):
         return MurnaghanOutput()
 
-    def _create_nodes(self):
-        cell = self.input.structure.get_cell()
-        nodes = []
-        for s in self.input.strains:
-            n = deepcopy(self.input.node)
-            n.input.structure = self.input.structure.copy()
-            n.input.structure.set_cell(cell * s, scale_atoms=True)
-            nodes.append(n)
-        return nodes
+    def _execute(self):
+        self.output.base_structure = self.input.structure
+        return super()._execute()
 
     def _extract_output(self, i, node, ret):
         if ret.is_done():

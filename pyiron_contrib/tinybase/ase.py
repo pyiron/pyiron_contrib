1a2b581a005e4b6485cc9f1e9d9d067b8b1f1d4a
Marvin Poul <ponder@creshal.de>
2023-03-06 13:22:49 +0100

    Add minimizer node for ASE

--- pyiron_contrib/tinybase/ase.py
+++ pyiron_contrib/tinybase/ase.py
@@ -1,11 +1,12 @@
 from .container import (
             AbstractInput,
+            StorageAttribute,
             StructureInput,
             MDInput,
             EnergyOutput,
             MDOutput
 )
-from . import AbstractNode
+from . import AbstractNode, ReturnStatus
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,6 +14,9 @@ import matplotlib.pyplot as plt
 from ase.md.langevin import Langevin
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase import units
+from ase.optimize.lbfgs import LBFGS
+from ase.optimize.fire import FIRE
+from ase.optimize.gpmin.gpmin import GPMin
 
 
 AseInput = AbstractInput.from_attributes('AseInput', 'calculator')
@@ -70,3 +74,69 @@ class AseMDNode(AbstractNode):
         parse()
         dyn.attach(parse, interval=self.input.steps // self.input.output_steps)
         dyn.run(self.input.steps)
+
+MinimizeInput = AbstractInput.from_attributes(
+        'MinimizeInput',
+        ionic_force_tolerance=float,
+        max_steps=int,
+        output_steps=int,
+)
+
+class AseMinimizeInput(AseInput, StructureInput, MinimizeInput):
+
+    """My first experimental docstring"""
+    algo = StorageAttribute().type(str).default('LBFGS')
+    """My second experimental docstring"""
+    minimizer_kwargs = StorageAttribute().type(dict).default(dict)
+
+    def lbfgs(self, damping=None, alpha=None):
+        self.algo = 'LBFGS'
+        if damping is not None:
+            self.minimizer_kwargs['damping'] = damping
+        if alpha is not None:
+            self.minimizer_kwargs['alpha'] = alpha
+
+    def fire(self):
+        self.algo = 'FIRE'
+
+    def gpmin(self):
+        self.algo = 'GPMIN'
+
+    def get_ase_optimizer(self, structure):
+        return {
+                'LBFGS': LBFGS,
+                'FIRE': FIRE,
+                'GPMIN': GPMin
+        }[self.algo](structure, **self.minimizer_kwargs)
+
+
+class AseMinimizeNode(AbstractNode):
+
+    def _get_input(self):
+        return AseMinimizeInput()
+
+    def _get_output(self):
+        return MDOutput()
+
+    def _execute(self):
+        structure = self.input.structure.copy()
+        structure.calc = self.input.calculator
+
+        opt = self.input.get_ase_optimizer(structure)
+
+        def parse():
+            self.output.structures.append(structure.copy())
+            self.output.pot_energies.append(structure.get_potential_energy())
+            self.output.kin_energies.append(structure.get_kinetic_energy())
+            self.output.forces.append(structure.get_forces())
+
+        opt.attach(parse, interval=self.input.output_steps)
+        opt.run(fmax=self.input.ionic_force_tolerance, steps=self.input.max_steps)
+        parse()
+
+        max_force = abs(self.output.forces[-1]).max()
+        if max_force > self.input.ionic_force_tolerance:
+            return ReturnStatus(
+                    "not_converged",
+                    f"force in last step ({max_force}) is larger than tolerance ({self.input.ionic_force_tolerance})!"
+            )

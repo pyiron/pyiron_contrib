c6490c425294f18d214ae47b275bd6fdf4989e9f
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2023-03-21 09:49:26 +0100

    Compatible to new coscine version?!

--- pyiron_contrib/generic/coscineIo.py
+++ pyiron_contrib/generic/coscineIo.py
@@ -3,17 +3,23 @@
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 import os.path
 import io
+import warnings
 
 import pandas as pd
 
 from pyiron_base import ImportAlarm
 from pyiron_base.interfaces.has_groups import HasGroups
 from pyiron_contrib.generic.filedata import StorageInterface
-from pyiron_contrib.generic.filedata import FileDataTemplate, load_file, MetaDataTemplate
+from pyiron_contrib.generic.filedata import (
+    FileDataTemplate,
+    load_file,
+    MetaDataTemplate,
+)
 from typing import Union, List
 
 try:
     import coscine
+
     import_alarm = ImportAlarm()
 except ImportError:
     import_alarm = ImportAlarm("Connecting to CoScInE requires the coscine package.")
@@ -22,20 +28,15 @@ except ImportError:
 
 class CoscineMetadata(coscine.resource.MetadataForm, MetaDataTemplate):
     """Add a proper representation to the coscine version"""
+
     def __init__(self, meta_data_form: coscine.resource.MetadataForm):
-        self._name = meta_data_form._name
-        self._lang = meta_data_form._lang
-        self._entries = meta_data_form._entries
-        self._keys = {}
-        for entry in meta_data_form._entries:
-            self._keys[entry["name"][self._lang]] = entry
-        self._vocabulary = meta_data_form._vocabulary
-        self.store = meta_data_form.store
-        self.profile = meta_data_form.profile
+        super().__init__(client=meta_data_form.client, graph=meta_data_form.profile)
 
     def to_dict(self):
-        result = {key: None for key in self.keys()}
-        result.update(self.store)
+        result = {}
+        for key, value in self.items():
+            if len(str(value)) > 0:
+                result[key] = value
         return result
 
     def __repr__(self):
@@ -43,7 +44,7 @@ class CoscineMetadata(coscine.resource.MetadataForm, MetaDataTemplate):
 
 
 class CoscineFileData(FileDataTemplate):
-    def __init__(self, coscine_object: coscine.Object):
+    def __init__(self, coscine_object: coscine.FileObject):
         super().__init__()
         self._coscine_object = coscine_object
         self._data = None
@@ -60,15 +61,27 @@ class CoscineFileData(FileDataTemplate):
         self._coscine_object.download(path=path)
 
     def _get_metadata(self):
-        form = CoscineMetadata(self._coscine_object.MetadataForm())
+        form = CoscineMetadata(self._coscine_object.form())
         return form
 
     def _set_metadata(self, metadata):
         raise AttributeError("can't set attribute")
 
 
+def _list_filter(list_to_filter, **kwargs):
+    """Return entry in list matching all attributes."""
+
+    def filter_func(item):
+        for key, value in kwargs:
+            if getattr(item, key) != value:
+                return False
+        return True
+
+    return list(filter(filter_func, list_to_filter))
+
+
 class CoscineResource(StorageInterface):
-    def __init__(self, resource: Union[coscine.Resource, dict, 'CoscineResource']):
+    def __init__(self, resource: Union[coscine.Resource, dict, "CoscineResource"]):
         """Giving access to a CoScInE Resource to receive or upload files
 
         Args:
@@ -78,13 +91,17 @@ class CoscineResource(StorageInterface):
         if isinstance(resource, coscine.Resource):
             self._resource = resource
         elif isinstance(resource, dict):
-            client = coscine.Client(resource['token'], verbose=False)
-            pr = client.projects(toplevel=False, id=resource['project_id'])[0]
-            self._resource = pr.resources(id=resource['resource_id'])[0]
+            client = coscine.Client(resource["token"])
+            pr = _list_filter(
+                client.projects(toplevel=False), id=resource["project_id"]
+            )[0]
+            self._resource = _list_filter(pr.resources(), id=resource["resource_id"])[0]
         elif isinstance(resource, self.__class__):
             self._resource = resource._resource
         else:
-            raise ValueError(f"Unknown resource type {type(resource)}, supported types !")
+            raise ValueError(
+                f"Unknown resource type {type(resource)}, supported types !"
+            )
 
     def _list_nodes(self):
         return [obj.name for obj in self._resource.objects()]
@@ -103,16 +120,21 @@ class CoscineResource(StorageInterface):
             return False
 
     def remove_file(self, item):
-        file_obj = self._get_one_file_obj(item, error_msg=f"Multiple matches found for item = {item}, aborting remove!")
+        file_obj = self._get_one_file_obj(
+            item,
+            error_msg=f"Multiple matches found for item = {item}, aborting remove!",
+        )
         if file_obj is not None:
             file_obj.delete()
 
-    def upload_file(self, file, metadata: coscine.resource.MetadataForm = None, filename=None):
+    def upload_file(
+        self, file, metadata: coscine.resource.MetadataForm = None, filename=None
+    ):
         _meta_data = self.validate_metadata(metadata)
         filename = filename or os.path.basename(file)
         self._resource.upload(filename, file, _meta_data)
 
-    def _get_one_file_obj(self, item, error_msg) -> Union[coscine.Object, None]:
+    def _get_one_file_obj(self, item, error_msg) -> Union[coscine.FileObject, None]:
         if item not in self.list_nodes():
             return None
 
@@ -122,7 +144,9 @@ class CoscineResource(StorageInterface):
         elif len(objects) > 1:
             raise ValueError(error_msg)
         else:
-            raise RuntimeError(f"This should be impossible. Please contact the developers.")
+            raise RuntimeError(
+                f"This should be impossible. Please contact the developers."
+            )
 
     def __getitem__(self, item):
         if item in self._list_nodes():
@@ -130,7 +154,7 @@ class CoscineResource(StorageInterface):
                 self._get_one_file_obj(
                     item,
                     f"Multiple matches for item = {item}, use `get_items(name={item})` to receive all matching files."
-                    f" See get_items docstring for more filtering options."
+                    f" See get_items docstring for more filtering options.",
                 )
             )
         raise ValueError(f"Unknown item {item}")
@@ -148,12 +172,17 @@ class CoscineResource(StorageInterface):
         Raises:
             ValueError: if 'name' and 'Name' are both given and do not match.
         """
-        _name = kwargs.pop('Name', None)
+        _name = kwargs.pop("Name", None)
         if name != _name:
-            raise ValueError(f"name='{name}' and Name='{_name}' both provided and not matching.")
+            raise ValueError(
+                f"name='{name}' and Name='{_name}' both provided and not matching."
+            )
 
-        kwargs['Name'] = name or _name
-        return [CoscineFileData(obj) for obj in self._resource.objects(**kwargs)]
+        kwargs["Name"] = name or _name
+        return [
+            CoscineFileData(obj)
+            for obj in _list_filter(self._resource.objects(), **kwargs)
+        ]
 
     @property
     def metadata_template(self) -> CoscineMetadata:
@@ -169,7 +198,7 @@ class CoscineResource(StorageInterface):
 
         # Try to update form by the dictionary:
         try:
-            form.update(metadata_dict)
+            form.fill(metadata_dict)
         except KeyError as e:
             store_err = KeyError(f"Meta data template has no key '{e.args[0]}'")
         else:
@@ -178,7 +207,7 @@ class CoscineResource(StorageInterface):
         # Assume we already got a dictionary from form.generate()
         # A form has the method parse(data) which is intended to be used on a coscine.Object.metadata which is the same
         # as {'file_handle_with_file_name': from.generate()} of the form used to upload; thus:
-        form.parse({'dummy_file_name': metadata_dict})
+        form.parse({"dummy_file_name": metadata_dict})
         return form, store_err
 
     def parse_metadata(self, metadata):
@@ -191,23 +220,27 @@ class CoscineResource(StorageInterface):
     def validate_metadata(self, metadata, raise_error=True):
         encountered_error = None
         if metadata is None and raise_error:
-            raise ValueError("Coscine resources require meta data to upload a file! Use metadata_template to "
-                             "get the correct meta data form and provide a completed form here.")
+            raise ValueError(
+                "Coscine resources require meta data to upload a file! Use metadata_template to "
+                "get the correct meta data form and provide a completed form here."
+            )
         elif metadata is None:
             return None
         elif isinstance(metadata, coscine.resource.MetadataForm):
             metadata_form = self.metadata_template
             metadata_form.clear()
-            metadata_form.update(metadata)
+            metadata_form.fill({k: v for k, v in metadata.items()})
         else:
             metadata_form, encountered_error = self._get_form_from_dict(metadata)
 
         try:
             result_meta_data = metadata_form.generate()
-        except coscine.RequirementError as e:
+        except ValueError as e:
             if raise_error and encountered_error is not None:
-                raise ValueError(f"The provided meta data is not valid; might be related to previous error "
-                                 f"{encountered_error}") from e
+                raise ValueError(
+                    f"The provided meta data is not valid; might be related to previous error "
+                    f"{encountered_error}"
+                ) from e
             elif raise_error:
                 raise ValueError(f"The provided meta data is not valid") from e
             else:
@@ -228,7 +261,7 @@ class CoscineProject(HasGroups):
             self._client = self._connect_client(token)
         elif isinstance(project, str):
             self._client = self._connect_client(project)
-        elif hasattr(project, 'client'):
+        elif hasattr(project, "client"):
             self._project = project
             self._client = project.client
         else:
@@ -241,20 +274,28 @@ class CoscineProject(HasGroups):
 
     @staticmethod
     def _connect_client(token):
-        client = coscine.Client(token, verbose=False)
+        client = coscine.Client(token)
         try:
             client.projects()
-        except coscine.CoscineException as e:
+        except (PermissionError, RuntimeError, ConnectionError) as e:
             raise ValueError("Error connecting to CoScInE with provided token.") from e
         return client
 
     @property
+    def read_only(self):
+        return self._client.settings.read_only
+
+    @read_only.setter
+    def read_only(self, val: bool):
+        self._client.settings.read_only = val
+
+    @property
     def verbose(self):
-        return self._client.verbose
+        return self._client.settings.verbose
 
     @verbose.setter
     def verbose(self, val):
-        self._client.verbose = val
+        self._client.settings.verbose = val
 
     def _list_groups(self):
         if self._project is None:
@@ -281,7 +322,13 @@ class CoscineProject(HasGroups):
 
     def get_group(self, key):
         if key in self.list_groups() and self._project is not None:
-            return self.__class__(self._project.subprojects(displayName=key)[0])
+            try:
+                return self.__class__(self._project.subproject(display_name=key))
+            except IndexError:
+                warnings.warn("More than one project matches - returning first match!")
+                for _pr in self._project.subprojects():
+                    if _pr.display_name == key:
+                        return self.__class__(_pr)
         elif key in self.list_groups():
             return self.__class__(self._client.project(key))
         else:

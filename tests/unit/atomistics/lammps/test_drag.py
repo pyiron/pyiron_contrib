7dc18f496aee4dfa65f24004b3bd6b28d04fd76b
samwaseda <o.waseda@mpie.de>
2023-07-26 07:50:35 +0000

    add one test

---
+++ tests/unit/atomistics/lammps/test_drag.py
@@ -0,0 +1,17 @@
+import unittest
+from pyiron_atomistics import Project
+from pyiron_contrib.atomistics.lammps.drag import setup_lmp_input
+
+class TestDrag(unittest.TestCase):
+    def setUp(self):
+        pr = Project("DRAG")
+        bulk = pr.create.structure.bulk('Ni', cubic=True)
+        self.lmp = pr.create.job.Lammps("test")
+        self.lmp.structure = bulk.repeat(2)
+
+    def test_calc_minimize(self):
+        self.assertRaises(ValueError, setup_lmp_input, self.lmp)
+
+
+if __name__ == '__main__':
+    unittest.main()

50bb8b07d20660fa7427c92bdd29f2c1eb39eb78
samwaseda <o.waseda@mpie.de>
2023-07-26 08:01:00 +0000

    add min_style test

--- tests/unit/atomistics/lammps/test_drag.py
+++ tests/unit/atomistics/lammps/test_drag.py
@@ -5,12 +5,15 @@ from pyiron_contrib.atomistics.lammps.drag import setup_lmp_input
 class TestDrag(unittest.TestCase):
     def setUp(self):
         pr = Project(".")
-        bulk = pr.create.structure.bulk('Ni', cubic=True)
         self.lmp = pr.create.job.Lammps("test")
-        self.lmp.structure = bulk.repeat(2)
+        self.lmp.structure = pr.create.structure.bulk('Ni', cubic=True)
 
     def test_calc_minimize(self):
         self.assertRaises(ValueError, setup_lmp_input, self.lmp)
+
+    def test_min_style(self):
+        sef.lmp.calc_minimize()
+        self.assertEqual(self.lmp.input.control["min_style"], "quickmin")
 
 
 if __name__ == '__main__':

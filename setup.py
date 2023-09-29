62d9abfd421f1f6908f8c5ef0e60c82a73bf755b
liamhuber <liamhuber@greyhavensolutions.com>
2023-09-29 11:14:03 -0700

    Update pyiron too

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=[        
         'matplotlib==3.7.2',
         'numpy==1.24.3',
-        'pyiron_base==0.6.3',
+        'pyiron_base==0.6.6',
         'scipy==1.11.2',
         'seaborn==0.12.2',
         'pyparsing==3.0.9',
@@ -40,7 +40,7 @@ setup(
     extras_require={
         'atomistic': [
             'ase==3.22.1',
-            'pyiron_atomistics==0.3.0',
+            'pyiron_atomistics==0.3.3',
             'pycp2k==0.2.2',
         ],
         'executors': [

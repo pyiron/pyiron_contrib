1b291fcf5607d5d0a6451eb6d51bf43db5429bcc
Leimeroth <leimeroth@mm.tu-darmstadt.de>
2023-11-06 08:43:13 +0100

    whitespace

--- pyiron_contrib/atomistics/atomicrex/potential_factory.py
+++ pyiron_contrib/atomistics/atomicrex/potential_factory.py
@@ -113,7 +113,7 @@ class AbstractPotential(DataContainer):
 
     def _plot_final_potential(self):
         raise NotImplementedError("Should be implemented in the subclass")
-    
+
 
 class BOPAbstract(AbstractPotential):
     def __init__(self, init=None, elements=None, export_file=None, identifier=None):
@@ -560,7 +560,7 @@ class EAMlikeMixin:
         for functions in self._function_tuple:
             for f in functions.values():
                 f.copy_final_to_initial_params(filter_func=filter_func)
-                
+
     def randomize_parameters(self, rng, filter_func=None):
         for functions in self._function_tuple:
             for f in functions.values():
